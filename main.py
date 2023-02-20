f852ffc741c9fbd433459fafba07461764cd4925
pavlogradskiy <122556174+pavlogradskiy@users.noreply.github.com>
2023-02-20 20:08:57 +0300

    Update main.py
    TOKEN_GROUP = 'vk1.a.Ba - TOKEN_GROUP'
    TOKEN_USER = 'vk1.a.08 - TOKEN_USER'

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from vk_api.keyboard import VkKeyboard
 import DbVk
 from MyVkApi import MyVkApi
 
-TOKEN_GROUP = 'vk1.a.BaRMc0B3Rxao6hmhEQXPbiOOhjKczaQExo7I11kEnE02uQFHBKRadvFVeTw7-g7dho2acXqaYG1OQZ1_eY9GgAAwS82WwUGhqVNQxYJC0_NDIM9AUEGRzxTdCkr0gFfuKWuZhXLb1o1PdUa16KkPmcxl8MaK0CMS8_6dLY_nTBTXSk6NGbfAW6xd4vpxxUdO5QSOFr9NIGmpZ6DADolf8g'
-TOKEN_USER = 'vk1.a.08f5CMXPa7yNWuzUhbGJebNPVARciBCnOnGR0cn5jkvMRgGsI4aEEym5oOMacTcX-TPnAQhz5Kt0-c9adGx8d-m-k6CSqcZl9cpCvHOovoZIhHeKETfYHvZYKiZhZyEx1sEpCR7jlFW_yx8ttipaBpEC4kwV4JVNiUUgSqYgEUZauRRVotuLXJ9Uxiz7dYZWquBfOpDNhZqmH8aZDBK6vQ'
+TOKEN_GROUP = 'vk1.a.Ba - TOKEN_GROUP'
+TOKEN_USER = 'vk1.a.08 - TOKEN_USER'
 RELATION_DICT = {
     '1': 'не женат/не замужем',
     '2': 'есть друг/есть подруга',
